95a8f5629d8aadefe75c27bc6550c80bb86ed4f8
Cece Yu <ceceyu@gmail.com>
2012-09-03 21:46:15 -0700

    first commit

---
+++ media/js/tipsy_skinny_nav.js
@@ -0,0 +1,8 @@
+$(function() {
+   $('#listing_things').tipsy({fade: true, gravity: 'w'});
+   $('#very_strategic').tipsy({fade: true, gravity: 'w'});
+   $('#artsy').tipsy({fade: true, gravity: 'w'});
+   $('#literate').tipsy({fade: true, gravity: 'w'});
+   $('#listening').tipsy({fade: true, gravity: 'w'});
+   $('#me').tipsy({fade: true, gravity: 'w'});
+ });
